25204db272ccc3d4dca56a419f1f21693889c278
Тамир Болданов <rpp-2011@yandex.ru>
2024-01-25 12:43:07 +0300

    after-review

--- src/main/java/ru/practicum/shareit/request/service/ItemRequestServiceImpl.java
+++ src/main/java/ru/practicum/shareit/request/service/ItemRequestServiceImpl.java
@@ -15,7 +15,6 @@ import ru.practicum.shareit.request.model.ItemRequestSort;
 import ru.practicum.shareit.request.repository.ItemRequestRepository;
 import ru.practicum.shareit.user.repository.UserRepository;
 
-import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
